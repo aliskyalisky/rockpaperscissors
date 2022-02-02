2a30a8b2d35519af9f4ba2509639256fc2a2edee
aliskyalisky <aleksi.liski@gmail.com>
2022-02-02 16:40:09 +0200

    Updated UI

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ let botScore = 0;
 let botSelection;
 let playerSelection;
 
+const btn = document.querySelectorAll('#btn');
+const scoreDisplayYou = document.querySelector('.scoredisplayyou');
+const scoreDisplayBot = document.querySelector('.scoredisplaybot');
+const alertBox = document.querySelector('.alertbox');
+
+scoreDisplayYou.textContent = '0';
+scoreDisplayBot.textContent = '0';
+
+
+for (let i = 0 ; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => {
+        if (i === 0) {
+            playerSelection = 'rock';
+        } else if (i === 1) {
+            playerSelection = 'paper';
+        } else if (i === 2) {
+            playerSelection = 'scissors';
+        }
+
+        playRound (playerSelection, botSelection)
+    });
+}
+
 
 function botPlay() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -16,30 +39,38 @@ function botPlay() {
     }
 
 function playRound (playerSelection, botSelection) {
+    if (playerScore == 5 || botScore == 5) {
+        playerScore = 0;
+        botScore = 0;
+    }
+
     botSelection = botPlay()
-    playerSelection = prompt(`Choose rock, paper or scissors. `).toLowerCase();
+    alertBox.textContent = ''
 
         if (playerSelection == 'rock' && botSelection == 'scissors' || playerSelection == 'paper' && botSelection == 'rock' || playerSelection == 'scissors' && botSelection == 'paper') {
             playerScore = ++playerScore;
-            alert(`You win! ${playerSelection} beats ${botSelection}. The score is ${playerScore}-${botScore}.`)
+            alertBox.textContent = `You win! ${playerSelection} beats ${botSelection}.`;
         } else if (playerSelection == 'rock' && botSelection == 'paper' || playerSelection == 'paper' && botSelection == 'scissors' || playerSelection == 'scissors' && botSelection == 'rock') {
             botScore = ++botScore;
-            alert(`You lose! ${botSelection} beats ${playerSelection}. The score is ${playerScore}-${botScore}.`)
+            alertBox.textContent = `You lose! ${botSelection} beats ${playerSelection}.`;
         } else {
-            alert(`It\'s a tie! You both chose ${playerSelection}. The score is ${playerScore}-${botScore}.`)
+            alertBox.textContent = `It\'s a tie! You both chose ${playerSelection}.`;
         }
 
-        if (playerScore < 5 && botScore < 5) {
-            playRound (playerSelection, botSelection)
-        } else if (playerScore == 5) {
-            alert('Game over, You win!!!')
-        } else if (botScore == 5) {
-            alert('Game over, You lost. :(')
-        } else {
-            alert('an error occured, please refresh the page.')
+        scoreDisplayYou.textContent = `${playerScore}`
+        scoreDisplayBot.textContent = `${botScore}`
+
+
+        if (playerScore == 5) {
+            alertBox.textContent = 'YOU WIN! Choose any option to play again.'
         }
+         if (botScore == 5) {
+            alertBox.textContent = 'You lost. Choose any option to try again.'
+        } 
+    
     }
+
     
-    alert('Let\'s play a game of Rock paper scissors. First to 5 wins.')
     
-    playRound (playerSelection, botSelection)
+    
+    
